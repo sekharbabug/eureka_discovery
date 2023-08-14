43630a59945c7c77fcadaa320f68282e013b266c
sekharbabug <sekhar.babu260@gmail.com>
2023-08-14 23:27:45 +0530

    Update PaymentController.java

--- payment-service/src/main/java/com/javatechie/spring/eureaka/provider/api/controller/PaymentController.java
+++ payment-service/src/main/java/com/javatechie/spring/eureaka/provider/api/controller/PaymentController.java
@@ -4,9 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @RestController
 @RequestMapping("/payment-provider")
+@EnableDiscoveryClient
 public class PaymentController {
 
 	@GetMapping("/payNow/{price}")
