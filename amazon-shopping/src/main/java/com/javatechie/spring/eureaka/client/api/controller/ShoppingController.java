e026380cbd17e27a55c33456b4127c5204d1e3ed
sekharbabug <sekhar.babu260@gmail.com>
2023-08-14 11:44:18 +0530

    Update ShoppingController.java

--- amazon-shopping/src/main/java/com/javatechie/spring/eureaka/client/api/controller/ShoppingController.java
+++ amazon-shopping/src/main/java/com/javatechie/spring/eureaka/client/api/controller/ShoppingController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 
 @RestController
