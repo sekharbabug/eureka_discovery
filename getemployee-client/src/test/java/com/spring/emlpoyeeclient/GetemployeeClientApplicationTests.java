d4218f7f5ced11a5c877484759d56f83cb72abcf
sekharbabug <sekhar.babu260@gmail.com>
2023-08-15 12:50:31 +0530

    Update GetemployeeClientApplicationTests.java

--- getemployee-client/src/test/java/com/spring/emlpoyeeclient/GetemployeeClientApplicationTests.java
+++ getemployee-client/src/test/java/com/spring/emlpoyeeclient/GetemployeeClientApplicationTests.java
@@ -1,12 +1,9 @@
 package com.spring.emlpoyeeclient;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GetemployeeClientApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
